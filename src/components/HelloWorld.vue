36bdb00d259fe58fc40df5be64164111e9835a59
chenyao0910 <chenyao0910@smail.nchu.edu.tw>
2024-07-15 00:36:51 +0800

    add dark mode

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,37 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {useDark, useToggle} from '@vueuse/core'
+import SvgIcon from "./SvgIcon.vue";
 
-defineProps<{ msg: string }>()
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="flex flex-col dark:bg-black bg-white h-screen">
+    <div  class="flex flex-row-reverse h-10">
+      <label class="inline-flex items-center cursor-pointer px-4">
+        <input type="checkbox" @click="toggleDark()" class="sr-only peer">
+        <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+      </label>
+    </div>
+    <div class="flex flex-1 justify-center items-center bg-white dark:bg-black">
+      <div class="text-center text-black dark:text-white">
+          I'm Yao Chen.
+        <div class="flex flex-row gap-x-16">
+          <a href="https://www.linkedin.com/in/yao-chen-937015168/">
+            <svg-icon class="w-6 h-6" name="linkedin"></svg-icon>
+          </a>
+          <a href="https://github.com/chenyao0910">
+            <svg-icon class="w-6 h-6" name="github2"></svg-icon>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<style >
+
 </style>
