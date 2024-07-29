335046fbea9c69cfcecccb6b048281817a889917
chenyao0910 <chenyao0910@smail.nchu.edu.tw>
2024-07-29 20:37:54 +0800

    rename helloworld to homepage

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld/>
+  <HomePage/>
 </template>
 
 <style>
